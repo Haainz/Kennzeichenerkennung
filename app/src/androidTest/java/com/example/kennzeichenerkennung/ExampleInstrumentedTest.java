479fb1c544fd63244b2d380d98c1bacd2dbb8ad4
Haainz <137401940+Haainz@users.noreply.github.com>
2025-04-24 12:23:38 +0200

    Update ExampleInstrumentedTest.java
    updated package name

--- app/src/androidTest/java/com/example/kennzeichenerkennung/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/kennzeichenerkennung/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.kennzeichenerkennung;
+package de.haainz.kennzeichenerkennung;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.kennzeichenerkennung", appContext.getPackageName());
+        assertEquals("de.haainz.kennzeichenerkennung", appContext.getPackageName());
     }
 }
